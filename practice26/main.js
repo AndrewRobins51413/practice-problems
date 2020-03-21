c850bb5d15423bbec2802f99953fdb7ceee20a0e
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-03-21 17:22:24 +0000

    practice practice!

---
+++ practice26/main.js
@@ -0,0 +1,9 @@
+var haystack = ['cat', 'dog', 'bird', 'turtle', 'lizard'];
+var needle = ['dog', 'lizard', 'flower', 'monkey', 'unicorn'];
+var output = [];
+
+for (var i=0; i<haystack.length; i++) {
+  for (var j=0; j<needle.length; j++) {
+
+  }
+}
