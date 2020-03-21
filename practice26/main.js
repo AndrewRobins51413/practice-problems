17e1c348c826035304dc4525d303ecc43f04db97
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-03-21 17:37:02 +0000

    practice practice

--- practice26/main.js
+++ practice26/main.js
@@ -2,8 +2,17 @@ var haystack = ['cat', 'dog', 'bird', 'turtle', 'lizard'];
 var needle = ['dog', 'lizard', 'flower', 'monkey', 'unicorn'];
 var output = [];
 
-for (var i=0; i<haystack.length; i++) {
-  for (var j=0; j<needle.length; j++) {
 
+function findAll(array1, array2){
+for (var i=0; i<array1.length; i++) {
+  for (var j=0; j<array2.length; j++) {
+
+    if (array1[i] === needle[j]) {
+      output.push(array1[i]);
+    }
   }
+  console.log('output', output);
 }
+}
+
+findAll(haystack, needle)
